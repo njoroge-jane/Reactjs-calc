4ca555ffe24403cd9f318bd3d3386df71176bef8
janey <jwnjoroge4@gmail.com>
2022-05-29 03:41:23 +0300

    create digits and buttons basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+<div className="calc">
+  <div className="display">
+    <span></span>0
+  </div>
+  <div className="operators">
+    <button>/</button>
+    <button>*</button>
+    <button>+</button>
+    <button>-</button>
+    <button>DEL</button>
+  </div>
+</div>
     </div>
   );
 }
